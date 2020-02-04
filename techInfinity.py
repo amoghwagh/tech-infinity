bb78caf03e987bf389cfeca82ee3ba37b9bbce0d
Amogh Wagh <amoghwagh@gmail.com>
2020-02-04 19:43:26 +0530

    Added jinja2 template

--- techInfinity.py
+++ techInfinity.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Amogh Wagh',
+        'title' : 'Blog Post 1',
+        'content' : 'First Post content',
+        'date_posted' : 'April 20 , 2019'
+    },
+     {
+        'author': 'Jane Doe',
+        'title' : 'Blog Post 2',
+        'content' : 'Second Post content',
+        'date_posted' : 'July 20 , 2019'
+    }
+]
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
